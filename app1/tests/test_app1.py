888b0d51ac21cf9d54ae20cac86ce3ffe0f5e864
kirankalsi <kiran.1998@hotmail.co.uk>
2020-12-02 11:05:05 +0000

    all tests except app1 mocking working

--- app1/tests/test_app1.py
+++ app1/tests/test_app1.py
@@ -1,6 +1,7 @@
-import unittest
+from unittest.mock import patch
 from flask import url_for
 from flask_testing import TestCase
+import requests_mock
 
 from application import app, db
 from application.models import Vehicles
@@ -20,7 +21,7 @@ class TestBase(TestCase):
         db.drop_all()
         db.create_all()
         
-        db_info = Vehicles(manufacturer='Audi', vehicle_type='Sports Car', total_price='50000')
+        db_info = Vehicles(manufacturer='Audi', vehicle_type='Sports Car', total_price='64500')
         db.session.add(db_info)
         db.session.commit()
 
@@ -28,9 +29,32 @@ class TestBase(TestCase):
         db.session.remove()
         db.drop_all()
 
-
 class TestViews(TestBase):
 
-    def test_index_get(self):
+    def test_index_view(self):
         response = self.client.get(url_for('index'))
         self.assertEqual(response.status_code, 200)
+
+class TestResponse(TestBase):
+    def test_vehicle(self):
+        with self.client:
+            with requests_mock.Mocker() as m:
+                m.get('http://34.105.145.24:5001/manufacturer', text='Audi')
+                m.get('http://34.105.145.24:5002/vehicle_type', text='Sports Car')
+                m.post('http://34.105.145.24:5003/total_price', text='64500')                
+                response = self.client.get(url_for('index'))
+                self.assertEqual(response.status_code, 200)
+
+'''
+    def test_vehicle(self):
+        with patch('requests.get') as g:
+            with patch('requests.post') as p:
+                g.return_value_manufacturer.text = "Audi"
+                g.return_value_vehicle_type.text = "Sports Car"
+                p.return_value_total_price.text = "64500"
+
+                response = self.client.get(url_for('index'))
+                self.assertIn(b'Audi Sports Car', response.data)
+                self.assertIn('The price is £64500', response.data)
+                self.assertEqual(response.status_code, 200)
+'''
--- app4/tests/test_app4.py
+++ app4/tests/test_app4.py
@@ -8,7 +8,38 @@ class TestBase(TestCase):
     def create_app(self):
         return app
 
-class TestManufacturerPrice(TestBase):
+class TestPrice(TestBase):
+    def test_total_price_audi_hatchback(self):
+        response=self.client.post('/total_price', json={'manufacturer': 'Audi', 'vehicle_type': 'Hatchback'})
+        self.assertIn(b'45499', response.data)
+
+    def test_total_price_bmw_roadster(self):
+        response=self.client.post('/total_price', json={'manufacturer': 'BMW', 'vehicle_type': 'Roadster'})
+        self.assertIn(b'57050', response.data)
+
+    def test_total_price_porsche_pickup_truck(self):
+        response=self.client.post('/total_price', json={'manufacturer': 'Porsche', 'vehicle_type': 'Pickup Truck'})
+        self.assertIn(b'107888', response.data)
+
+    def test_total_price_ford_sports_car(self):
+        response=self.client.post('/total_price', json={'manufacturer': 'Ford', 'vehicle_type': 'Sports Car'})
+        self.assertIn(b'31500', response.data)
+
+    def test_total_price_ferrari_saloon(self):
+        response=self.client.post('/total_price', json={'manufacturer': 'Ferrari', 'vehicle_type': 'Saloon'})
+        self.assertIn(b'163333', response.data)
+
+    def test_total_price_lamborghini_convertible(self):
+        response=self.client.post('/total_price', json={'manufacturer': 'Lamborghini', 'vehicle_type': 'Convertible'})
+        self.assertIn(b'171500', response.data)
+
+    def test_total_price_toyota_supermini(self):
+        response=self.client.post('/total_price', json={'manufacturer': 'Toyota', 'vehicle_type': 'Supermini'})
+        self.assertIn(b'36499', response.data)
+
+
+
+'''    
     def test_man_price_audi(self):
         response = self.client.post(
             url_for('total_price'),
@@ -16,15 +47,5 @@ class TestManufacturerPrice(TestBase):
             follow_redirects=True
         )
         self.assertIn(b'45499' or '56550' or '53388' or '64500' or '47833' or '66000' or '54499', response.data)
-'''
-    def test_man_price_audi(self):
-        with patch('requests.get') as g:
-            with patch('requests.post') as p:
-                g.return_value.text = "Audi"
-                p.return_value.text = 44500
 
-                response = self.client.get(url_for('total_price'))
-                self.assertIn(b'Audi', response.data)
-                #self.assertIn(b'12', response.data)
-                #self.assertEqual(response.status_code, 200)
 '''
