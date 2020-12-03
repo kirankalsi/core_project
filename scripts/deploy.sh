c3922adddb3e4c23ac223c7425717d89788a41ae
kirankalsi <kiran.1998@hotmail.co.uk>
2020-12-03 15:12:45 +0000

    pull images

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,10 @@
 
 ssh kiran11kalsi@34.105.145.24 << EOF
 
+docker pull kiran11kalsi/app1:latest
+docker pull kiran11kalsi/app2:latest
+docker pull kiran11kalsi/app3:latest
+docker pull kiran11kalsi/app4:latest
 cd core_project
 git pull
 docker stack deploy --compose-file docker-compose.yaml core_project_stack
