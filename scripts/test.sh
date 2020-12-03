bbef83018464e9db3530c65c4d7b7334f7ab5c8d
kirankalsi <kiran.1998@hotmail.co.uk>
2020-12-03 10:24:24 +0000

    apt-get

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 sudo apt update
-sudo apt install python3 python3-pip python3-venv
+sudo apt-get install python3 python3-pip python3-venv
 python3 -m venv proj_venv
 . proj_venv/bin/activate
 pip3 install -r app1/requirements.txt app2/requirements.txt app3/requirements.txt app4/requirements.txt
