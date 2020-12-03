a9abdad1b87f8442e2e99e50fd2ecd7854cb3e57
kirankalsi <kiran.1998@hotmail.co.uk>
2020-12-03 10:54:26 +0000

    tests run in container

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
+docker build -t testing-image -f testing/Dockerfile .
+docker run -it -d --name testing-container testing-image
+
+docker exec testing-container pytest ./app1 --cov ./app1/application/
+docker exec testing-container pytest ./app2 --cov ./app2/application/
+docker exec testing-container pytest ./app3 --cov ./app3/application/
+docker exec testing-container pytest ./app4 --cov ./app4/application/
+
+docker stop testing-container
+docker rm testing-container
+
+'''
 sudo apt update
 sudo apt-get install python3 python3-pip python3-venv
 python3 -m venv proj_venv
@@ -13,3 +25,4 @@ pytest ./app4 --cov ./app4/application
 
 deactivate
 rm -rf proj_venv
+'''
