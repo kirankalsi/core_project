5f0c3ee178adff47aa4e962cf127b0004e6e5297
kirankalsi <kiran.1998@hotmail.co.uk>
2020-12-03 16:09:45 +0000

    changed env variables location

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,7 @@ ssh kiran11kalsi@34.105.145.24 << EOF
 
 cd core_project
 git pull
-docker stack deploy --compose-file docker-compose.yaml core_project_stack
+echo ${DB_URI}
+env DB_URI=${DB_URI} env MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} docker stack deploy --compose-file docker-compose.yaml core_project_stack
 
 EOF
--- scripts/push.sh
+++ scripts/push.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 
 docker-compose push
-env DB_URI=${DB_URI} env MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
