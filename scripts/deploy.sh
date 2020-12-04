d1fa1808f73648f08d622ae7e14d4580c253cc5e
kirankalsi <kiran.1998@hotmail.co.uk>
2020-12-04 00:27:12 +0000

    added .env

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ ssh kiran11kalsi@34.105.145.24 << EOF
 
 cd core_project
 git pull
-echo ${DB_URI}
-env DB_URI=${DB_URI} env MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} docker stack deploy --compose-file docker-compose.yaml core_project_stack
+. .env
+env DB_URI=${DB_URI} env SECRET_KEY=${SECRET_KEY} env MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} docker stack deploy --compose-file docker-compose.yaml core_project_stack
 
 EOF
