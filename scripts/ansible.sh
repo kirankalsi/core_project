101560c59c50f13ca85461697b66a25b8036736e
kirankalsi <kiran.1998@hotmail.co.uk>
2020-12-04 12:40:53 +0000

    pip install

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+sudo apt install python3-pip
 mkdir -p ~/.local/bin
 echo 'PATH=$PATH:home/jenkins/.local/bin' >> ~/.bashrc
 source ~/.bashrc
+pip3 install --user ansible
 
 git clone https://github.com/kirankalsi/core_project.git
 cd core_project
