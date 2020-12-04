71d42cf6f736daf02965574aa263bb7bae698a8a
kirankalsi <kiran.1998@hotmail.co.uk>
2020-12-04 13:37:26 +0000

    small change

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-sudo apt install python3-pip
-mkdir -p ~/.local/bin
-echo 'PATH=$PATH:home/jenkins/.local/bin' >> ~/.bashrc
-source ~/.bashrc
-pip3 install --user ansible
-
-git clone https://github.com/kirankalsi/core_project.git
-cd core_project
-git checkout ansible
+echo ${PATH}
 ansible-playbook -i inventory playbook.yaml
